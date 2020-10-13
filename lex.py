1b5a5f992232a3b27fd0c09d47bf930d3de6c40c
Lucas Dutra <ldutra98@gmail.com>
2020-10-13 19:43:28 -0300

    Solve exercise

--- lex.py
+++ lex.py
@@ -13,4 +13,21 @@ def lex(code: str) -> Iterable[Token]:
     da string de código fornecida.
     """
 
+    tokens = [
+        ("STRING", r"\".*\""),
+        ("NUMBER", r"(\+|\-|)\d+(\.\d*)?"),
+        ("NAME", r"([a-zA-Z_%\+\-]|\.\.\.)[a-zA-Z_0-9\-\>\?\!]*"),
+        ("CHAR", r"#\\[a-zA-Z]*"),
+        ("BOOL", r"#[t|f]"),
+        ("QUOTE", r"\'"),
+        ("LPAR", r"\("),
+        ("RPAR", r"\)"),
+    ]
+
+    code = re.sub(r";;.*", "", code)
+    regex = '|'.join('(?P<%s>%s)' % pair for pair in tokens)
+
+    for token in re.finditer(regex, code):
+        yield Token(token.lastgroup, token.group())
+
     return [Token('INVALIDA', 'valor inválido')]
